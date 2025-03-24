69ddb00c31801beef1b5ab3f851788c59f0f69e9
M-Talha-1 <mtalhaasifshazad@gmail.com>
2025-03-24 09:58:49 +0500

    Set Up CORS

--- app/main.py
+++ app/main.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 from app.routers import resume  
 from app.routers import job
 from app.routers import matching
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Update this for production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
